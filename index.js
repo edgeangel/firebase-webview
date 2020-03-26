a4749d0c5eb3a341783ac29984eeee79ca81884f
Mathieu Lima <mathieu.lima@gmail.com>
2020-03-26 18:54:38 +0100

    split html, css, js

---
+++ index.js
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2020 EdgeAngel
+ * 
+ * Licensed under GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * https://www.gnu.org/licenses/gpl-3.0.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+ 
+ function logEvent(name, params) {
+  if (!name) {
+    return;
+  }
+
+  if (window.AnalyticsWebInterface) {
+    // Call Android interface
+    window.AnalyticsWebInterface.logEvent(name, JSON.stringify(params));
+  } else if (window.webkit
+      && window.webkit.messageHandlers
+      && window.webkit.messageHandlers.firebase) {
+    // Call iOS interface
+    var message = {
+      command: 'logEvent',
+      name: name,
+      parameters: params
+    };
+    window.webkit.messageHandlers.firebase.postMessage(message);
+  } else {
+    // No Android or iOS interface found
+    console.log("No native APIs found.");
+  }
+}
+
+function setUserProperty(name, value) {
+  if (!name || !value) {
+    return;
+  }
+
+  if (window.AnalyticsWebInterface) {
+    // Call Android interface
+    window.AnalyticsWebInterface.setUserProperty(name, value);
+  } else if (window.webkit
+      && window.webkit.messageHandlers
+      && window.webkit.messageHandlers.firebase) {
+    // Call iOS interface
+    var message = {
+      command: 'setUserProperty',
+      name: name,
+      value: value
+   };
+    window.webkit.messageHandlers.firebase.postMessage(message);
+  } else {
+    // No Android or iOS interface found
+    console.log("No native APIs found.");
+  }
+}
+
+document.getElementById("user-property").addEventListener("click", function() {
+  console.log("user-property");
+  setUserProperty("user_status", "unlogged");
+  setUserProperty("user_platform", "app_webview");
+});
+
+document.getElementById("event-1").addEventListener("click", function() {
+  console.log("event-1");
+  logEvent("custom_event", { id: 1, source: "webview" });
+});
+
+document.getElementById("event-2").addEventListener("click", function() {
+  console.log("event-2");
+  logEvent("custom_event", { id: 2, source: "webview" });
+});
